f8712ebba3aed57f3232ce759429d35a850fe411
Konstantin Kovar <schwammerlkopf@hotmail.com>
2022-05-17 10:24:07 +0200

    added content to the footer

--- src/components/Footer/Footer.svelte
+++ src/components/Footer/Footer.svelte
@@ -1,1 +1,16 @@
-<footer class="h-4 p-2" />
+<footer
+  class="mt-8 h-4 border-t-2 border-stone-600 pt-4 text-center font-serif text-xs text-stone-600"
+>
+  <p>
+    I am keeping all the cookies to myself and you can't
+    have <span class="underline">any</span>.
+  </p>
+  <p>
+    This website uses <a
+      target="_blank"
+      href="https://www.goatcounter.com/"
+      >a very privacy aware analytics tool</a
+    >.
+  </p>
+  <p class="mb-4">Do something nice today.</p>
+</footer>
