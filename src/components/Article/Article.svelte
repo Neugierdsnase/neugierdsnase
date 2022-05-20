083847559b9daee6aff7ab2d20aca30bdc13e1a0
Konstantin Kovar <schwammerlkopf@hotmail.com>
2022-05-20 11:31:57 +0200

    fixed CLS

--- src/components/Article/Article.svelte
+++ src/components/Article/Article.svelte
@@ -5,9 +5,9 @@
 <svelte:window bind:scrollY={y} />
 
 <article
-  class="prose prose-stone mx-auto mt-4 prose-code:font-mono prose-code:text-lg prose-code:font-normal before:prose-code:hidden after:prose-code:hidden xl:prose-xl 2xl:prose-2xl"
+  class="prose prose-stone mx-auto mt-4 translate-y-96 prose-code:font-mono prose-code:text-lg prose-code:font-normal before:prose-code:hidden after:prose-code:hidden xl:prose-xl 2xl:prose-2xl"
   style={`transform: ${
-    y < 450 ? `translateY(${450 - y}px)` : 'translate(0px)'
+    y > 450 ? 'translate(0px)' : `translateY(${450 - y}px)`
   };`}
 >
   <slot />
